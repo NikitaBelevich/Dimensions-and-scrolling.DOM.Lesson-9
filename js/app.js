6ba308c696eb1380319a400a4004e3410dbf4dae
Nikita Belevich <belevish_1998@mail.ru>
2020-04-19 13:35:09 +0300

    Задача 1 (Выполнено)
    Сколько прокручено от низа элемента, реверс scrollTop

--- js/app.js
+++ js/app.js
@@ -1,2 +1,24 @@
 'use strict';
 
+//TODO Task 1
+const btn1 = document.querySelector('.task1 button');
+btn1.addEventListener('click', scrollFromBottom);
+
+function scrollFromBottom() {
+    const scrollBlock1 = document.querySelector('.scroll-block-1');
+
+    let scrollTop = scrollBlock1.scrollTop;
+    let scrollHeight = scrollBlock1.scrollHeight;
+    let clientHeight = scrollBlock1.clientHeight;
+
+    const scrollBottom = scrollHeight - scrollTop - clientHeight;
+    
+    console.log('Прокручено от низа', scrollBottom);
+}
+//TODO Task 1
+
+
+
+
+
+
